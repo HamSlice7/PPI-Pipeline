c0f1e473fef214d14e195040c85dc6b3563fee78
Jacob Hambly <148881284+HamSlice7@users.noreply.github.com>
2024-06-24 08:13:10 -0400

    Update min_peptidase_inhibitor_distance.py

--- min_peptidase_inhibitor_distance.py
+++ min_peptidase_inhibitor_distance.py
@@ -4,6 +4,11 @@ import numpy as np
 parser = PDB.PDBParser(QUIET = True)
 
 def min_distance(pdb_file, active_site_residue_num):
+    """
+    Input: PDB file for a peptidase-inhibitor complex (string), a residue number for the active site (int)
+    Output: The minimum distance between the alpha carbon of the active site residue and a alpha carbon of the inhibitor (float)
+    """
+    
     #create a structure object from pdb_file
     structure = parser.get_structure("complex", pdb_file)
 
