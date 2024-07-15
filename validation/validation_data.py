7c267aa4e184dfe669a6eb431f8a6235a0d6ea52
HamSlice7 <jhambly@uoguelph.ca>
2024-07-15 15:27:42 -0400

    updating validation_data.py to ensure there are no duplicate peptidase-inhibitor complexes in the negative data

--- validation/validation_data.py
+++ validation/validation_data.py
@@ -25,14 +25,22 @@ df_validation_positive = df_validation_positive.drop('True', axis=1)
 inhibitor_families = df_validation_sampled['Inhibitor Family'].unique()
 
 random_pairs = []
-# Generate a new dataframe with randomized peptidase-inhibitor pairs that are not from the same family
-for row in range(df_validation_sampled.shape[0]):
-    peptidase = df_validation_sampled['Peptidase'][row]
-    peptidase_family = df_validation_sampled['Peptidase Family'][row]
-    inhibitor_family = random.choice([fam for fam in inhibitor_families if fam != peptidase_family])
-    inhibitor = df_validation_sampled[df_validation_sampled['Inhibitor Family'] == inhibitor_family]['Inhibitor'].sample(n=1, random_state = seed).values[0]
-    random_pairs.append({'Peptidase': peptidase, 'Peptidase Family': peptidase_family, 'Inhibitor': inhibitor, 'Inhibitor Family': inhibitor_family})
 
+inhibitors_negative_set = []
+count = 0
+# Generate a new dataframe with randomized peptidase-inhibitor pairs that are not from the same family
+while count < 20:
+    peptidase = df_validation_sampled['Peptidase'][count]
+    peptidase_family = df_validation_sampled['Peptidase Family'][count]
+    inhibitor_family = random.choice([fam for fam in inhibitor_families if fam != peptidase_family])
+    inhibitor = df_validation_sampled[df_validation_sampled['Inhibitor Family'] == inhibitor_family]['Inhibitor'].sample(n=1).values[0]
+    new_pair = ({'Peptidase': peptidase, 'Peptidase Family': peptidase_family, 'Inhibitor': inhibitor, 'Inhibitor Family': inhibitor_family})
+    if new_pair not in random_pairs:
+        random_pairs.append(new_pair)
+        count += 1
+    else:
+        pass
+            
 df_validation_negative = pd.DataFrame(random_pairs)
 
 df_validation_final = pd.concat([df_validation_positive, df_validation_negative], ignore_index=True, axis=0)
